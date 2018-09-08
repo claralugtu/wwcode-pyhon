2a8459ab5e81746d8726415fa1db3e686f6c7847
claralugtu <30742502+claralugtu@users.noreply.github.com>
2018-09-08 16:45:30 +0800

    add template
    add proper template and description for the challenge

--- firstChallenge.py
+++ firstChallenge.py
@@ -1,3 +1,12 @@
+""" The Challenge
+Author:
+Description: Aling Nena’s Sari-sari store wants a robot that will ask the
+customer their total bill and payment amount and tell them their change
+(for now, we can allow negative change).
+"""
+
+# Build your code below
+
 bill = input('How much is your total bill?')
 payment = input('How much is your payment?')
 
